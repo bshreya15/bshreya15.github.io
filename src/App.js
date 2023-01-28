0d13890ba109fd3921c59730242227f6b684c4a7
Shreya Bakshi <i.bakshi.shreya@gmail.com>
2023-01-28 23:48:19 +0530

    feat. header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import React from "react";
+// import { ReactFragment } from "react";
+import "./App.css";
+import Header from "./components/header/Header";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
-}
+};
 
 export default App;
