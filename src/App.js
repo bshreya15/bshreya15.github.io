7ec707d86fae03a29a12161ca615605c65e517a5
Shreya Bakshi <i.bakshi.shreya@gmail.com>
2023-09-01 18:58:33 +0530

    feat skills skeleton

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Skills from "./components/skills/Skills";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <main className="main">
         <Home />
+        <Skills />
       </main>
     </>
   );
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,15 @@
+import "./skills.css";
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+      <div className="skills__container container grid">
+        <div className="skills__content grid">
+          <Skills />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
