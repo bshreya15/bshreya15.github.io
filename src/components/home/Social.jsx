d50110df08614641fa8c75fadca63c03cfea2cd7
Shreya Bakshi <i.bakshi.shreya@gmail.com>
2023-09-04 10:36:29 +0530

    feat attr className

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -2,21 +2,21 @@ import React from "react";
 
 const Social = () => {
   return (
-    <div class="home__social">
+    <div className="home__social">
       <a
         href="https://github.com/bshreya15"
-        class="home__social-icon"
+        className="home__social-icon"
         target="blank"
       >
-        <i class="uil uil-github-alt"></i>
+        <i className="uil uil-github-alt"></i>
       </a>
 
       <a
         href="https://www.linkedin.com/in/bakshi-shreya/"
-        class="home__social-icon"
+        className="home__social-icon"
         target="blank"
       >
-        <i class="uil uil-linkedin-alt"></i>
+        <i className="uil uil-linkedin-alt"></i>
       </a>
     </div>
   );
