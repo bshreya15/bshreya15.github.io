14ee26fea84139f1d8761c1391615282b4dbbcf2
Shreya Bakshi <i.bakshi.shreya@gmail.com>
2023-09-05 21:33:26 +0530

    feat skill section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+// import Skills from "./components/skills/Skills";
+import Skills2 from "./components/skills/Skills2";
 
 const App = () => {
   return (
@@ -10,6 +12,7 @@ const App = () => {
       <Header />
       <main className="main">
         <Home />
+        <Skills2 />
       </main>
     </>
   );
---
+++ src/components/skills/Skills2.jsx
@@ -0,0 +1,225 @@
+import React from "react";
+import "./skills.css";
+
+const Skills2 = () => {
+  return (
+    <section className="skills section" id="skills">
+      <h3 className="skills__heading">Technical Proficiencies</h3>
+      <div className="skills__container container slider">
+        <div className="skills__content slide-track">
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/react-native.png"
+              alt="react-native"
+            />
+          </div>
+          <div className="slide">
+            <img src="https://img.icons8.com/color/144/redux.png" alt="redux" />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/nodejs.png"
+              alt="nodejs"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/javascript--v1.png"
+              alt="javascript--v1"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/typescript.png"
+              alt="typescript"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/ios-filled/150/jquery.png"
+              alt="jquery"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/html-5--v1.png"
+              alt="html-5--v1"
+            />
+          </div>
+          <div className="slide">
+            <img src="https://img.icons8.com/color/144/css3.png" alt="css3" />
+          </div>
+          <div className="slide">
+            <img src="https://img.icons8.com/color/144/sass.png" alt="sass" />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/bootstrap.png"
+              alt="bootstrap"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/java-coffee-cup-logo--v1.png"
+              alt="java"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/python--v1.png"
+              alt="python"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-jest-can-collect-code-coverage-information-from-entire-projects-logo-color-tal-revivo.png"
+              alt="jest"
+            />
+          </div>
+          {/* duplicate */}
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/react-native.png"
+              alt="react-native"
+            />
+          </div>
+          <div className="slide">
+            <img src="https://img.icons8.com/color/144/redux.png" alt="redux" />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/nodejs.png"
+              alt="nodejs"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/javascript--v1.png"
+              alt="javascript--v1"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/typescript.png"
+              alt="typescript"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/ios-filled/150/jquery.png"
+              alt="jquery"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/html-5--v1.png"
+              alt="html-5--v1"
+            />
+          </div>
+          <div className="slide">
+            <img src="https://img.icons8.com/color/144/css3.png" alt="css3" />
+          </div>
+          <div className="slide">
+            <img src="https://img.icons8.com/color/144/sass.png" alt="sass" />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/bootstrap.png"
+              alt="bootstrap"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/java-coffee-cup-logo--v1.png"
+              alt="java"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/python--v1.png"
+              alt="python"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-jest-can-collect-code-coverage-information-from-entire-projects-logo-color-tal-revivo.png"
+              alt="jest"
+            />
+          </div>
+          {/* part3 */}
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/react-native.png"
+              alt="react-native"
+            />
+          </div>
+          <div className="slide">
+            <img src="https://img.icons8.com/color/144/redux.png" alt="redux" />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/nodejs.png"
+              alt="nodejs"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/javascript--v1.png"
+              alt="javascript--v1"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/typescript.png"
+              alt="typescript"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/ios-filled/150/jquery.png"
+              alt="jquery"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/html-5--v1.png"
+              alt="html-5--v1"
+            />
+          </div>
+          <div className="slide">
+            <img src="https://img.icons8.com/color/144/css3.png" alt="css3" />
+          </div>
+          <div className="slide">
+            <img src="https://img.icons8.com/color/144/sass.png" alt="sass" />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/bootstrap.png"
+              alt="bootstrap"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/java-coffee-cup-logo--v1.png"
+              alt="java"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/color/144/python--v1.png"
+              alt="python"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-jest-can-collect-code-coverage-information-from-entire-projects-logo-color-tal-revivo.png"
+              alt="jest"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills2;
