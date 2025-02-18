850ad7f95149d6c6670832f0ded2b93f4b6cb375
shreya <i.bakshi.shreya@gmail.com>
2025-02-18 15:52:58 +0000

    feat navbar

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,27 +1,20 @@
 "use client";
 
 import React from "react";
-
-// import { useEffect } from 'react';
-import { motion } from "framer-motion"; 
+import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
 import clsx from "clsx";
-import { useActiveSectionContext } from "@/context/active-section-context"; 
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Header() {
   const { activeSection, setActiveSection, setTimeOfLastClick } =
-  useActiveSectionContext();
-
-// useEffect(() => {
-//   // Force scroll to top on page load
-//   window.scrollTo(0, 0);
-// }, []);
+    useActiveSectionContext();
 
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-full border sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:bg-opacity-65  border-white/15  bg-white/10 backdrop-blur"
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-full border sm:top-6 sm:h-[3.25rem] sm:w-[31rem] sm:rounded-full dark:bg-gray-950 dark:bg-opacity-65  border-white/15  bg-white/10 backdrop-blur"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
@@ -37,10 +30,8 @@ export default function Header() {
             >
               {/*  dark:text-gray-400 dark:hover:text-gray-300 font-semibold */}
               <Link
-                className=
-                {clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:hover:text-gray-300 "
-                  ,
+                className={clsx(
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:hover:text-gray-300 ",
                   {
                     "text-white dark:text-white-200":
                       activeSection === link.name,
@@ -55,19 +46,21 @@ export default function Header() {
                 {link.name}
 
                 {link.name === activeSection && (
-                <motion.span className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
-                layoutId="activeSection"
-                transition={{
-                  type: "spring",
-                  stiffness: 380,
-                  damping: 30,
-                }}></motion.span>
+                  <motion.span
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                  ></motion.span>
                 )}
               </Link>
             </motion.li>
           ))}
         </ul>
-      </nav> 
+      </nav>
     </header>
   );
 }
