69c2592ca7c2cde58b2274fbac353cefc88a6018
shreya <i.bakshi.shreya@gmail.com>
2025-02-18 15:53:21 +0000

    feat. hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,12 +1,25 @@
-"use-client";
+"use client";
 import MotionBg from "@/components/MotionHeroBg";
 import MotionImage from "@/components/MotionHeroImage";
 import MotionLinks from "@/components/MotionHeroLinks";
 import MotionText from "@/components/MotionHeroText";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useEffect } from "react";
 
 export const HeroSection = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.3,
+  });
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("Home");
+    }
+  }, [inView]);
   return (
-    <section id="home">
+    <section id="home" ref={ref}>
       <div className="py-24 md:py-32 md:mt-8 lg:py-28 lg:mt-12 relative overflow-x-clip">
         <MotionBg />
         <div className="container">
